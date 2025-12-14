f6fefc397d688c8a4ad9e7d3cdb62225f018830e
Steiz321 <fundamentdaiil@gmail.com>
2025-12-14 20:42:04 +0200

    PA #2

--- main.js
+++ main.js
@@ -4,47 +4,37 @@ let gl; // The webgl context.
 let surface; // A surface model
 let shProgram; // A shader program
 let spaceball; // A SimpleRotator object that lets the user rotate the view by mouse.
+let uSlider, vSlider; // Sliders for granularity
+let light_rotation_angle = 0;
+let lastTime = 0;
 
 function deg2rad(angle) {
   return (angle * Math.PI) / 180;
 }
 
-// corrugated sphere
-// classic sphere formula :
-// x = R0*sinΘcosφ, y =  R0*sinΘsinφ, z = R0cosΘ
-// corrugated sphere formula :
-// for creating "corrugation" we modify radius
-// R (Θ,φ) = R0 + Asin(kΘ)cos(mφ)
-// R0 - basic radius
-// A - corrugation amplitude (height of waves)
-// k and m - frequencies corrugation along ϑ and ϕ
-// The higher the number, the more “waves” there are on the surface.
-// x(Θ,φ) = (R0 + Asin(kΘ)cos(mφ))*sinΘcosφ
-// y(Θ,φ) = (R0 + Asin(kΘ)cos(mφ))*sinΘsinφ
-// z(Θ,φ) = (R0 + Asin(kΘ)cos(mφ))*cosΘ
-
 // Constructor
 function Model(name) {
   this.name = name;
   this.iVertexBuffer = gl.createBuffer();
+  this.iNormalBuffer = gl.createBuffer();
+  this.iIndexBuffer = gl.createBuffer();
   this.count = 0;
 
-  // -- new properties for saving parameters grid --
-  this.numSegments = 0;
-  this.numULines = 0;
-  this.numVLines = 0;
-  // ---------------
-
-  this.BufferData = function (vertices, numSegments, numULines, numVLines) {
+  this.BufferData = function (vertices, normals, indices) {
     gl.bindBuffer(gl.ARRAY_BUFFER, this.iVertexBuffer);
     gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STREAM_DRAW);
 
-    // -- saving parameters --
-    this.count = vertices.length / 3;
-    this.numSegments = numSegments;
-    this.numULines = numULines;
-    this.numVLines = numVLines;
-    // -----------------------
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(normals), gl.STREAM_DRAW);
+
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
+    gl.bufferData(
+      gl.ELEMENT_ARRAY_BUFFER,
+      new Uint16Array(indices),
+      gl.STREAM_DRAW
+    );
+
+    this.count = indices.length;
   };
 
   this.Draw = function () {
@@ -52,49 +42,36 @@ function Model(name) {
     gl.vertexAttribPointer(shProgram.iAttribVertex, 3, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(shProgram.iAttribVertex);
 
-    // -- logic for drawing --
-    const verticesPerLine = this.numSegments + 2;
-    // +2 due to the repetition of the first point to break
-    const totalLines = this.numULines + this.numVLines;
+    gl.bindBuffer(gl.ARRAY_BUFFER, this.iNormalBuffer);
+    gl.vertexAttribPointer(shProgram.iAttribNormal, 3, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(shProgram.iAttribNormal);
 
-    let offset = 0;
-    for (let i = 0; i < totalLines; i++) {
-      // draw each line separately
-      gl.drawArrays(gl.LINE_STRIP, 0, this.count);
-      offset += verticesPerLine;
-    }
-    // --------------------------
+    gl.bindBuffer(gl.ELEMENT_ARRAY_BUFFER, this.iIndexBuffer);
+    gl.drawElements(gl.TRIANGLES, this.count, gl.UNSIGNED_SHORT, 0);
   };
 }
 
 function ShaderProgram(name, program) {
   this.name = name;
   this.prog = program;
-
-  // Location of the attribute variable in the shader program.
   this.iAttribVertex = -1;
-  // Location of the uniform specifying a color for the primitive.
+  this.iAttribNormal = -1;
+  this.iModelViewMatrix = -1;
+  this.iProjectionMatrix = -1;
+  this.iNormalMatrix = -1;
+  this.iLightPos = -1;
   this.iColor = -1;
-  // Location of the uniform matrix representing the combined transformation.
-  this.iModelViewProjectionMatrix = -1;
 
   this.Use = function () {
     gl.useProgram(this.prog);
   };
 }
 
-/* Draws a colored cube, along with a set of coordinate axes.
- * (Note that the use of the above drawPrimitive function is not an efficient
- * way to draw with WebGL.  Here, the geometry is so simple that it doesn't matter.)
- */
 function draw() {
   gl.clearColor(0, 0, 0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
 
-  /* Set the values of the projection transformation */
   let projection = m4.perspective(Math.PI / 8, 1, 8, 12);
-
-  /* Get the view matrix from the SimpleRotator object.*/
   let modelView = spaceball.getViewMatrix();
 
   let rotateToPointZero = m4.axisRotation([0.707, 0.707, 0], 0.7);
@@ -103,126 +80,141 @@ function draw() {
   let matAccum0 = m4.multiply(rotateToPointZero, modelView);
   let matAccum1 = m4.multiply(translateToPointZero, matAccum0);
 
-  /* Multiply the projection matrix times the modelview matrix to give the
-       combined transformation matrix, and send that to the shader program. */
-  let modelViewProjection = m4.multiply(projection, matAccum1);
+  gl.uniformMatrix4fv(shProgram.iModelViewMatrix, false, matAccum1);
+  gl.uniformMatrix4fv(shProgram.iProjectionMatrix, false, projection);
 
-  gl.uniformMatrix4fv(
-    shProgram.iModelViewProjectionMatrix,
-    false,
-    modelViewProjection
-  );
+  let normalMatrix = m4.inverse(matAccum1);
+  normalMatrix = m4.transpose(normalMatrix);
+  gl.uniformMatrix4fv(shProgram.iNormalMatrix, false, normalMatrix);
 
-  /* Draw the six faces of a cube, with different colors. */
-  gl.uniform4fv(shProgram.iColor, [1, 1, 0, 1]); // Yellow color
+  const radius = 7;
+  const x = radius * Math.cos(light_rotation_angle);
+  const z = radius * Math.sin(light_rotation_angle);
+  gl.uniform3fv(shProgram.iLightPos, [x, 3.0, z]);
+
+  gl.uniform4fv(shProgram.iColor, [1, 1, 0, 1]);
 
   surface.Draw();
 }
 
-// -- updated fuction for corrugated sphere --
-function CreateSurfaceData() {
+function animate(time) {
+  const time_in_seconds = time / 1000;
+  const deltaTime = time_in_seconds - lastTime;
+  lastTime = time_in_seconds;
+
+  // Update animation state
+  const rotationSpeed = 0.5; // radians per second
+  light_rotation_angle += rotationSpeed * deltaTime;
+
+  draw(); // Render the scene
+  window.requestAnimationFrame(animate);
+}
+function CreateSurfaceData(u_lines, v_lines) {
   let vertexList = [];
+  let normalList = [];
+  let indicesList = [];
+
   const R0 = 1.5;
-  const A = 0.05; // -- corrugation amplitude
-  const k = 15; // -- vertical corrugation frequency (theta)
-  const m = 15; // -- horizontal corrugation frequency (pho=9)
-
-  const num_u_lines = 20; // -- number of lines V
-  const num_v_lines = 30; // -- number of lines U
-  const num_segments = 50; // -- number of segments per line
-
-  // -- function for calculation coord of corrugated sphere --
+  const A = 0.05;
+  const k = 15;
+  const m = 15;
+  const num_segments = 50;
   function get_corrugated_sphere_coords(theta, phi) {
-    // --parametric equation for a corrugated sphere --
-    // -- R(theta, phi) = R0 + A * sin(k * theta) * cos(m * phi) --
     let R = R0 + A * Math.sin(k * theta) * Math.cos(m * phi);
-
     let x = R * Math.sin(theta) * Math.cos(phi);
     let y = R * Math.sin(theta) * Math.sin(phi);
     let z = R * Math.cos(theta);
     return [x, y, z];
   }
+  const du = 0.001;
+  const dv = 0.001;
+  // Function to compute the normal vector at a point (theta, phi)
+  function get_normal(theta, phi) {
+    // Approximate partial derivatives using central differences
+    let P_u_plus = get_corrugated_sphere_coords(theta + du, phi);
+    let P_u_minus = get_corrugated_sphere_coords(theta - du, phi);
+    let P_v_plus = get_corrugated_sphere_coords(theta, phi + dv);
+    let P_v_minus = get_corrugated_sphere_coords(theta, phi - dv);
 
-  // -- for u lines --
-  // -- theta (Θ) from 0 to PI, phi (φ) from 0 to 2*PI --
-  for (let i = 0; i < num_u_lines; i++) {
-    let theta = (i * Math.PI) / (num_u_lines - 1);
+    let dP_du = m4
+      .subtractVectors(P_u_plus, P_u_minus)
+      .map((c) => c / (2 * du));
+    let dP_dv = m4
+      .subtractVectors(P_v_plus, P_v_minus)
+      .map((c) => c / (2 * dv));
 
-    // -- repeat first point, for breaking LINE_STRIP --
-    let coords_start = get_corrugated_sphere_coords(theta, 0);
-    vertexList.push(...coords_start); // -- first point --
-
-    for (let j = 0; j <= num_segments; j++) {
-      let phi = (j * 2 * Math.PI) / num_segments;
+    let normal = m4.cross(dP_du, dP_dv);
+    return m4.normalize(normal);
+  }
+  for (let i = 0; i <= u_lines; i++) {
+    let theta = (i * Math.PI) / u_lines;
+    for (let j = 0; j <= v_lines; j++) {
+      let phi = (j * 2 * Math.PI) / v_lines;
       let coords = get_corrugated_sphere_coords(theta, phi);
       vertexList.push(...coords);
+      let normal = get_normal(theta, phi);
+      normalList.push(...normal);
     }
-    vertexList.push(...coords_start); // -- end point (repeating) --
   }
 
-  // -- for v lines --
-  // -- phi from 0 to 2*PI, theta from 0 to PI --
-  for (let i = 0; i < num_v_lines; i++) {
-    let phi = (i * 2 * Math.PI) / (num_v_lines - 1);
-
-    let coords_start = get_corrugated_sphere_coords(0, phi);
-    vertexList.push(...coords_start); // -- first point --
-
-    for (let j = 0; j <= num_segments; j++) {
-      let theta = (j * Math.PI) / num_segments;
-      let coords = get_corrugated_sphere_coords(theta, phi);
-      vertexList.push(...coords);
+  for (let i = 0; i < u_lines; i++) {
+    for (let j = 0; j < v_lines; j++) {
+      let p1 = i * (v_lines + 1) + j;
+      let p2 = p1 + 1;
+      let p3 = (i + 1) * (v_lines + 1) + j;
+      let p4 = p3 + 1;
+      indicesList.push(p1, p2, p3);
+      indicesList.push(p2, p4, p3);
     }
-    vertexList.push(...coords_start); // -- end point (repeating) --
   }
 
-  // -- return list of vertices and parameters --
   return {
     vertices: vertexList,
-    numSegments: num_segments,
-    numULines: num_u_lines,
-    numVLines: num_v_lines,
+    normals: normalList,
+    indices: indicesList,
   };
 }
 
-/* Initialize the WebGL context. Called from init() */
+function updateSurface() {
+  const u = parseInt(uSlider.value);
+  const v = parseInt(vSlider.value);
+  const surfaceData = CreateSurfaceData(u, v);
+  surface.BufferData(
+    surfaceData.vertices,
+    surfaceData.normals,
+    surfaceData.indices
+  );
+  draw();
+}
+
 function initGL() {
   let prog = createProgram(gl, vertexShaderSource, fragmentShaderSource);
 
-  shProgram = new ShaderProgram("Basic", prog);
+  shProgram = new ShaderProgram("Phong", prog);
   shProgram.Use();
 
   shProgram.iAttribVertex = gl.getAttribLocation(prog, "vertex");
-  shProgram.iModelViewProjectionMatrix = gl.getUniformLocation(
-    prog,
-    "ModelViewProjectionMatrix"
-  );
+  shProgram.iAttribNormal = gl.getAttribLocation(prog, "normal");
+  shProgram.iModelViewMatrix = gl.getUniformLocation(prog, "ModelViewMatrix");
+  shProgram.iProjectionMatrix = gl.getUniformLocation(prog, "ProjectionMatrix");
+  shProgram.iNormalMatrix = gl.getUniformLocation(prog, "NormalMatrix");
+  shProgram.iLightPos = gl.getUniformLocation(prog, "light_position");
   shProgram.iColor = gl.getUniformLocation(prog, "color");
 
   surface = new Model("Surface");
-
-  // -- get data and grid parameters --
-  const surfaceData = CreateSurfaceData();
-
-  // -- pass all data to Model object --
+  const surfaceData = CreateSurfaceData(
+    parseInt(uSlider.value),
+    parseInt(vSlider.value)
+  );
   surface.BufferData(
     surfaceData.vertices,
-    surfaceData.numSegments,
-    surfaceData.numULines,
-    surfaceData.numVLines
+    surfaceData.normals,
+    surfaceData.indices
   );
 
   gl.enable(gl.DEPTH_TEST);
 }
 
-/* Creates a program for use in the WebGL context gl, and returns the
- * identifier for that program.  If an error occurs while compiling or
- * linking the program, an exception of type Error is thrown.  The error
- * string contains the compilation or linking error.  If no error occurs,
- * the program identifier is the return value of the function.
- * The second and third parameters are strings that contain the
- * source code for the vertex shader and for the fragment shader.
- */
 function createProgram(gl, vShader, fShader) {
   let vsh = gl.createShader(gl.VERTEX_SHADER);
   gl.shaderSource(vsh, vShader);
@@ -246,10 +238,12 @@ function createProgram(gl, vShader, fShader) {
   return prog;
 }
 
-/**
- * initialization function that will be called when the page has loaded
- */
 function init() {
+  uSlider = document.getElementById("u-slider");
+  vSlider = document.getElementById("v-slider");
+  uSlider.addEventListener("input", updateSurface);
+  vSlider.addEventListener("input", updateSurface);
+
   let canvas;
   try {
     canvas = document.getElementById("webglcanvas");
@@ -263,7 +257,7 @@ function init() {
     return;
   }
   try {
-    initGL(); // initialize the WebGL graphics context
+    initGL();
   } catch (e) {
     document.getElementById("canvas-holder").innerHTML =
       "<p>Sorry, could not initialize the WebGL graphics context: " +
@@ -274,5 +268,5 @@ function init() {
 
   spaceball = new TrackballRotator(canvas, draw, 0);
 
-  draw();
+  window.requestAnimationFrame(animate);
 }
